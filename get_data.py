3d7bce6dc48672c42ebfd6e6fc7196cdd82310b1
Hadrizia <hadrizia.santos@ccc.ufcg.edu.br>
2018-03-22 18:04:00 -0300

    Getting div parent

--- get_data.py
+++ get_data.py
@@ -9,10 +9,21 @@ def get_text(soup_entity):
     '''This function gets a clean version of the text from a soup entity.'''
     return ' '.join([line.strip() for line in soup_entity.get_text().strip().splitlines()]).lower()
 
-list = soup.find('div', attrs={'class': 'list_item odd'})
-for element in list.children:
-    print element
+list = soup.find('div', attrs={'class': 'list detail eplist'})
 
+for element in list.children:
+    # if img['alt'] != 'loading':
+  
+
+    img = element.find('img')
+    print (img)
+    # if img['alt'] != 'loading':
+    #     for children in img.children:
+    #          if children.name == 'div':
+    #             season, episode = children.text.split(', ')
+    #             print (season)
+    #             print (episode)
         
+
 
     
